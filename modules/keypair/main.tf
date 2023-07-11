bd8fc0fd4a8aad706c670a0ae7081677639a78c3
aamer <aamer@Ameers-MacBook-Air.local>
2023-07-11 21:25:55 +0530

    Updated

--- modules/keypair/main.tf
+++ modules/keypair/main.tf
@@ -1,21 +1,4 @@
-resource "tls_private_key" "pk" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
 resource "aws_key_pair" "kp" {
   key_name   = var.key_name
-  public_key = tls_private_key.pk.public_key_openssh
-}
-
-resource "local_file" "pem" {
-  content         = tls_private_key.pk.private_key_openssh
-  filename        = "temp/${var.key_name}"
-  file_permission = "0700"
-}
-
-resource "local_file" "pub" {
-  content         = aws_key_pair.kp.public_key
-  filename        = "temp/${var.key_name}.pub"
-  file_permission = "0700"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
--- temp/userdata.sh
+++ temp/userdata.sh
@@ -5,5 +5,5 @@ sudo apt upgrade -y
 sudo apt install awscli -y
 echo "alias k=kubectl">>/home/ubuntu/.bashrc
 echo "alias tf=terraform">>/home/ubuntu/.bashrc
-sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-00e74c2b55d52d73d.efs.ap-south-2.amazonaws.com:/ efs
+sudo reboot now
 echo "*** Completed Installing awscli2 ***"
